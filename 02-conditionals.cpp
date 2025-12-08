6951d0129f10ea94f1f730c2cc34774378b33f65
taco0cat <hashirhameed107@gmail.com>
2025-12-09 00:01:20 +0400

    Conditional, Ternary, Switches

--- 02-conditionals.cpp
+++ 02-conditionals.cpp
@@ -16,15 +16,49 @@ int main(){
         cout << "x is equal to y" << endl;
     }
 
+// --------------------------------------------------------------------------------- //
+
     // Ternary Operator
     int time1 = 20;
     // string result = (time1 < 18) ? "Good day." : "Good evening.";
-    cout << ((time1 < 18) ? "Good day." : "Good evening.");
+    cout << ((time1 < 18) ? "Good day." : "Good evening.") << endl;
 
     // Nested Ternary Operator
     int time2 = 22;
     string message = (time2 < 12) ? "Good morning." : (time2 < 18) ? "Good afternoon." : "Good evening.";
     cout << message;
+
+// --------------------------------------------------------------------------------- //
+
+    // Switches
+
+    int day = 4;
+    switch (day) {
+        case 1:
+            cout << "Monday";
+            break;
+        case 2:
+            cout << "Tuesday";
+            break;
+        case 3:
+            cout << "Wednesday";
+            break;
+        case 4:
+            cout << "Thursday";
+            break;
+        case 5:
+            cout << "Friday";
+            break;
+        case 6:
+            cout << "Saturday";
+            break;
+        case 7:
+            cout << "Sunday";
+            break;        
+        default:
+            cout << "Invalid day";
+            break;
+    }
 
     return 0;
 }
