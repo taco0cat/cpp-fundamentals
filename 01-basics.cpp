9df61f5c48bba0c25a010e6c760b2e3ab2b8f958
taco0cat <hashirhameed107@gmail.com>
2025-12-08 21:32:36 +0400

    Variables

--- 01-basics.cpp
+++ 01-basics.cpp
@@ -4,9 +4,34 @@
 using namespace std;
 
 int main() {
-    // Print to the console and end the line (alternative:"\n" )
-    cout << "Hello World!" << endl;
+    // Print to the console and end the line (alternative: endl)
+    cout << "Hello World!" << "\n\n";
 
+// --------------------------------------------------------------------------------- //
+
+    // Declaring Variables (Multiple) and Assigning Values
+    int x, y, z;                 // Integer
+    x = y = z = 50;
+
+    float myFloat = 19.23;       // Float (7 decimals)
+    double sciNo = 19.23e5;      // Double (15 decimals)
+    char myLetter = 'A';         // Character
+    string myText = "Taco0Cat";  // String
+    bool myBoolean = true;       // Boolean
+
+    // Constants
+    const int minutesPerHour = 60;
+
+    cout << sciNo << "\n\n";
+
+// --------------------------------------------------------------------------------- //
+
+    // User Input
+
+    string username; 
+    cout << "Enter your username: ";
+    cin >> username;
+    cout << "Hello " << username;
 
     return 0;
 }
