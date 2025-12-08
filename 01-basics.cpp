9d798fe4ccd7c69d5e2f746426ee158f06eb5e6d
taco0cat <hashirhameed107@gmail.com>
2025-12-08 20:30:50 +0400

    README

---
+++ 01-basics.cpp
@@ -0,0 +1,12 @@
+// imports Input/Output Stream library before compilation
+#include <iostream>
+// "std::cout <<" --> "cout <<"
+using namespace std;
+
+int main() {
+    // Print to the console and end the line (alternative:"\n" )
+    cout << "Hello World!" << endl;
+
+
+    return 0;
+}
